9a18b4f27ad5054d9df100da5433a1786d63e78e
carlos <carlos@LAPTOP-9Q6APE6P>
2021-10-15 13:15:07 -0500

    EF Core & Database First & MVC Scaffolding

---
+++ S5_MVCEFCore.DatabaseFirst/Models/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+#nullable disable
+
+namespace S5_MVCEFCore.DatabaseFirst.Models
+{
+    public partial class Customer
+    {
+        public Customer()
+        {
+            Order = new HashSet<Order>();
+        }
+
+        public int Id { get; set; }
+        //Se usa para personalisar (NO ES NECESARIO)
+        [Display(Name = "Nombres")]
+        public string FirstName { get; set; }
+        //Se usa para personalisar (NO ES NECESARIO)
+        [Display(Name = "Apellidos")]
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+
+        public virtual ICollection<Order> Order { get; set; }
+    }
+}
